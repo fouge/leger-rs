a28fde54aa2519a2bd2cb182511a053236f60ebb
Cyril Fougeray <cyril.fougeray@gmail.com>
2020-11-30 12:41:19 +0100

    For later, an effective test would be to launch a TCP server to test that part.

--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,11 @@
 extern crate std;
 
-use crate::PolkaProvider;
-
-#[test]
-fn new_test() {
-
-}
+// use crate::PolkaProvider;
+//
+// #[test]
+// fn new_test() {
+// 	// Instanciate a TCP server
+// 	// connect to the server
+// 	// send a request, echo the request
+// 	// read back the response.
+// }
