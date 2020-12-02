1903062871ee33e3dfaa9fc65ab4428cfee3ec94
Cyril Fougeray <cyril.fougeray@gmail.com>
2020-12-02 10:45:56 +0100

    Error if parsing fails

--- src/account/tests.rs
+++ src/account/tests.rs
@@ -8,11 +8,13 @@ fn test_new_account() {
 	let mut seed:[u8; 32] = [0_u8; 32];
 	hex::decode_to_slice(
 		"ad282c9eda80640f588f812081d98b9a2333435f76ba4ad6258e9c6f4a488363",
-		&mut seed as &mut [u8]);
+		&mut seed as &mut [u8])
+		.expect("Cannot decode hex string");
 	let mut account_id:[u8; 32] = [0_u8; 32];
 	hex::decode_to_slice(
 		"f6a7ac42a6e1b5329bdb4e63c8bbafa5301add8102843bfe950907bd3969d944",
-		&mut account_id as &mut [u8]);
+		&mut account_id as &mut [u8])
+		.expect("Cannot decode hex string");
 
 	let mut public = [0_u8; 32];
 	let mut private = [0_u8; 64];
