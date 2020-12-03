c5aaa849ba0bbf154a5d684ab048e114e954afb8
Cyril Fougeray <cyril.fougeray@gmail.com>
2020-12-03 10:36:33 +0100

    Storing public key as ss58 string. Added tests for account creation

--- src/account/mod.rs
+++ src/account/mod.rs
@@ -1,14 +1,44 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![no_builtins]
+
 #[cfg(test)]
 mod tests;
 
 use ed25519_compact::{KeyPair, Seed, Noise};
+use crate::Provider;
+use core::str;
+use heapless::{String, Vec, consts::*};
+use serde::de::value::U64Deserializer;
+use blake2::{Blake2b, Blake2s, Digest};
+
+const PREFIX: &[u8] = b"SS58PRE";
 
 /// Key type is an array of 32 bytes
 pub type Key = [u8; 32];
 
+enum AccountError {
+	CannotFetchAccountInfo,
+	CannotConvert,
+}
+
+pub struct AccountInfo {
+	nonce: u64,
+	ref_count: u64,
+	data: AccountData
+}
+
+pub struct AccountData {
+	free: u64,
+	reserved: u64,
+	misc_frozen: u64,
+	free_frozen: u64,
+}
+
 pub struct Account {
 	/// Public (account ID) and secret keys are stored into the `KeyPair`
 	keys: KeyPair,
+	info: Option<AccountInfo>,
+	ss58: String<U64>,
 }
 
 impl Account {
@@ -18,11 +48,52 @@ impl Account {
 		// Generates a new key pair using private key as seed.
 		let key_pair = KeyPair::from_seed(Seed::new(private_key));
 
-		Account{ keys: key_pair }
+		let mut body = [0_u8; 35];
+		let mut output = [0_u8; 64];
+
+		// concatenate address type and public key
+		// address-Type is Generic Substrate wildcard
+		body[0] = 0x2A;
+		body[1..].iter_mut()
+			.zip(key_pair.pk.iter())
+			.for_each(|(f, t)| *f = *t);
+
+		let mut hasher = Blake2b::new();
+		hasher.update(PREFIX);
+		hasher.update(body[0..33].as_ref());
+		let hash = hasher.finalize();
+
+		body[33..].iter_mut().zip(hash.iter())
+			.for_each(|(f, t)| *f = *t);
+
+		let l = bs58::encode(body.as_ref()).into(&mut output[..]).unwrap();
+		let v: Vec<u8, U64> = Vec::from_slice(output[..l].as_ref()).unwrap();
+		let s: String<U64> = String::from_utf8(v).unwrap();
+
+		Account { keys: key_pair, info: None, ss58: s }
 	}
 
 	pub fn sign_tx(&self, msg: &mut [u8]) {
 		let signed = self.keys.sk.sign(&msg, Some(Noise::default()));
 		msg.copy_from_slice(signed.as_ref());
+	}
+
+	pub fn ss58(&self) -> &str {
+		self.ss58.as_str()
+	}
+
+	pub fn to_u8a(&self) -> &str {
+		"not implemented"
+	}
+
+	fn get_info<S>(&mut self, provider: &mut Provider<S>) -> Result<AccountInfo, AccountError> {
+		provider.rpc.rpc_method(Some("state_getStorage"), Some("0xNone"));
+
+		Err(AccountError::CannotFetchAccountInfo)
+	}
+
+	fn get_balance<S>(&mut self, provider: &mut Provider<S>) -> Option<u64> {
+		//self.get_balance(provider)?;
+		Some(0_u64)
 	}
 }
--- src/account/tests.rs
+++ src/account/tests.rs
@@ -18,10 +18,50 @@ fn test_new_account() {
 
 	let mut public = [0_u8; 32];
 	let mut private = [0_u8; 64];
-	public.copy_from_slice(Account::new(seed).keys.pk.as_ref());
-	private.copy_from_slice(Account::new(seed).keys.sk.as_ref());
+
+	let account = Account::new(seed);
+	public.copy_from_slice(account.keys.pk.as_ref());
+	private.copy_from_slice(account.keys.sk.as_ref());
 
 	assert_eq!(public, account_id);
 }
 
-// TODO add test sign_tx
+#[test]
+fn test_new_account_2() {
+	// mnemonic
+	// smoke key grief belt gather absurd open attend keep flip hollow popular
+
+	let mut seed:[u8; 32] = [0_u8; 32];
+	hex::decode_to_slice(
+		"DA5CE9BB3618B9004F0D76C0FE97DA6C075AE60937FC7B3A8C01A16A655E9388",
+		&mut seed as &mut [u8])
+		.expect("Cannot decode hex string");
+	let mut account_id:[u8; 32] = [0_u8; 32];
+	hex::decode_to_slice(
+		"B10355577E96846A5C2144BCC37108BAD78538CB7D11EA0456D041190A19A0B7",
+		&mut account_id as &mut [u8])
+		.expect("Cannot decode hex string");
+
+	let mut public = [0_u8; 32];
+	let mut private = [0_u8; 64];
+
+	let account = Account::new(seed);
+	public.copy_from_slice(account.keys.pk.as_ref());
+	private.copy_from_slice(account.keys.sk.as_ref());
+
+	assert_eq!(public, account_id);
+	assert_eq!(account.ss58(), "5G4oKgivg3aY1bkai9sFdPPV8LVL5pQi5gYgmic6qbL4y5Wm")
+}
+
+#[test]
+fn test_to_ss58() {
+	let mut seed:[u8; 32] = [0_u8; 32];
+	hex::decode_to_slice(
+		"ad282c9eda80640f588f812081d98b9a2333435f76ba4ad6258e9c6f4a488363",
+		&mut seed as &mut [u8])
+		.expect("Cannot decode hex string");
+
+	let account = Account::new(seed);
+	let ss58 = account.ss58();
+	assert_eq!(ss58, "5He7SpmVsdhoEKC5uwvPDngoCXECCeh8VrxoxnQTh1mgPiZa")
+}
